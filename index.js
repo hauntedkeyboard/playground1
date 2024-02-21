b7ca1ad3f8164483874f8ca0ea9388c781b203c7
hauntedkeyboard <118305462+hauntedkeyboard@users.noreply.github.com>
2024-02-22 00:19:03 +0400

    experimenting with string slicing

--- index.js
+++ index.js
@@ -21,6 +21,13 @@ myButton.onclick = function() {
     label3.textContent = randomNum3;
 };
 
-let myString = "   All work and no play makes Jack a dull boy.    ";
-charLabel.innerText = myString.replaceAll('dull boy', 'promising young man').padEnd(180, "All work and all play baby. ...").repeat(75);
-console.log(myString.replaceAll("dull boy", "sick fuck"));
+let myString = "All work and no play makes Jack a dull boy.    ";
+// charLabel.innerText = myString.replaceAll('dull boy', 'promising young man').padEnd(180, "All work and all play baby. ...").repeat(75);
+// console.log(myString.replaceAll("dull boy", "sick fuck"));
+
+let name = myString.trim().slice(27, 32);
+let lastChar = myString.trim().slice(0);
+let firstWord = myString.trim().slice(0, myString.indexOf(" "));
+let lastWord = myString.slice(myString.trim().lastIndexOf(" "));
+charLabel.innerText = lastWord;
+
