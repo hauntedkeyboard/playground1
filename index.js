abdbeec5e3051580b2a4853f1a1dc1c6393f023d
hauntedkeyboard <118305462+hauntedkeyboard@users.noreply.github.com>
2024-02-24 23:08:10 +0400

    add description for  the number guessing game, comment out the rest of the code

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
-const myButton = document.getElementById('myButton');
-const label1 = document.getElementById('myLabel1');
-const label2 = document.getElementById('myLabel2');
-const label3 = document.getElementById('myLabel3');
-const min = 2;
-const max = 10;
-let randomNum1;
-let randomNum2;
-let randomNum3;
-const charLabel = document.getElementById('myCharLabel');
+
+/* A GAME WHERE COMPUTER TRIES TO GUESS A NUMBER    
+STEP 1. YOU COME UP WITH A NUMBER 
+    make a const to store it
+STEP 2. COMPUTER MAKES A GUESS AND WAITS FOR YOUR REPLY
+STEP 3.A. YOU CONFIRM THE CORRECT GUESS - COMPUTER CONGRATULATES ITSELF
+STEP 3.B. YOU SAY THAT THE NUMBER IS BIGGER
+    COMPUTER UPDATES PARAMETERS, MAKES ANOTHER GUESS AND RETURNS TO THE END OF 2.
+STEP 3.C. YOU SAY THAT THE NUMBER IS SMALLER 
+    COMPUTER UPDATES PARAMETERS, MAKES ANOTHER GUESS AND RETURNS TO THE END OF 2.
 
 
-myButton.onclick = function() {
-    randomNum1 = Math.floor(Math.random() * (max+1-min) + min);
-    randomNum2 = Math.floor(Math.random() * (max+1-min) + min);
-    randomNum3 = Math.floor(Math.random() * (max+1-min) + min);
 
 
-    label1.textContent = randomNum1;
-    label2.textContent = randomNum2;
-    label3.textContent = randomNum3;
-};
+// const myButton = document.getElementById('myButton');
+// const label1 = document.getElementById('myLabel1');
+// const label2 = document.getElementById('myLabel2');
+// const label3 = document.getElementById('myLabel3');
+// const min = 2;
+// const max = 10;
+// let randomNum1;
+// let randomNum2;
+// let randomNum3;
+// const charLabel = document.getElementById('myCharLabel');
 
-let myString = "All work and no play makes Jack a dull boy.    ";
-// charLabel.innerText = myString.replaceAll('dull boy', 'promising young man').padEnd(180, "All work and all play baby. ...").repeat(75);
-// console.log(myString.replaceAll("dull boy", "sick fuck"));
 
-let name = myString.trim().slice(27, 32);
-let lastChar = myString.trim().slice(0);
-let firstWord = myString.trim().slice(0, myString.indexOf(" "));
-let lastWord = myString.slice(myString.trim().lastIndexOf(" "));
-charLabel.innerText = lastWord;
+// myButton.onclick = function() {
+//     randomNum1 = Math.floor(Math.random() * (max+1-min) + min);
+//     randomNum2 = Math.floor(Math.random() * (max+1-min) + min);
+//     randomNum3 = Math.floor(Math.random() * (max+1-min) + min);
+
+
+//     label1.textContent = randomNum1;
+//     label2.textContent = randomNum2;
+//     label3.textContent = randomNum3;
+// };
+
+// let myString = "All work and no play makes Jack a dull boy.    ";
+// // charLabel.innerText = myString.replaceAll('dull boy', 'promising young man').padEnd(180, "All work and all play baby. ...").repeat(75);
+// // console.log(myString.replaceAll("dull boy", "sick fuck"));
+
+// let name = myString.trim().slice(27, 32);
+// let lastChar = myString.trim().slice(0);
+// let firstWord = myString.trim().slice(0, myString.indexOf(" "));
+// let lastWord = myString.slice(myString.trim().lastIndexOf(" "));
+// charLabel.innerText = lastWord;
 
