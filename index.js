7e785185d10435629187d55c5af1a9ba11e0b92a
hauntedkeyboard <118305462+hauntedkeyboard@users.noreply.github.com>
2024-03-05 00:10:47 +0400

    experiment with rest parameters

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
 /* A GAME WHERE COMPUTER TRIES TO GUESS A NUMBER    
-STEP 1. YOU COME UP WITH A NUMBER FROM 1 - 100
+STEP 1. YOU COME UP WITH A NUMBER FROM 1 - 100 AND TYPE IT INTO A FIELD
     make a const to store it
-STEP 2. COMPUTER MAKES A GUESS AND WAITS FOR YOUR REPLY
-    a variable - computer outputs random number within the current parameters 
+    after you click on 'Sumbit'
+STEP 2. COMPUTER MAKES AN INITIAL GUESS AND WAITS FOR YOUR REPLY
+    a variable - computer outputs random number within the current parameters into a field
+    YOU ARE PRESENTED WITH THREE BUTTONS TO CHOOSE 
 STEP 3.A. YOU CONFIRM THE CORRECT GUESS 
-    computer congratulates itself
+    congratulates itself
     asks you if you want to make a guess again
 STEP 3.B. YOU SAY THAT THE NUMBER IS BIGGER
     COMPUTER UPDATES PARAMETERS, 
@@ -15,36 +17,40 @@ STEP 3.C. YOU SAY THAT THE NUMBER IS SMALLER
     MAKES ANOTHER GUESS AND RETURNS TO THE END OF 2.*/
 
 
-const textField = document.getElementById("myLabel1");
-const btnTooHigh = document.getElementById("btnTooHigh");
-const btnTooLow = document.getElementById("btnTooLow");
-const btnWon = document.getElementById("btnWon");
-const mySumbit = document.getElementById("mySubmit");
-let guessed = false;
-let currentMin = 1;
-let currentMax = 100;
+// const textField = document.getElementById("myLabel1");
+// const btnTooHigh = document.getElementById("btnTooHigh");
+// const btnTooLow = document.getElementById("btnTooLow");
+// const btnWon = document.getElementById("btnWon");
+// const mySumbit = document.getElementById("mySubmit");
+// let guessed = false;
+// let currentMin = 1;
+// let currentMax = 100;
 
 
-function getRandomNumberMinMax(){
-    return Math.floor(Math.random * (currentMax - currentMin) + currentMin + 1);
-};
+// function getRandomNumberMinMax(){
+//     return Math.floor(Math.random() * (currentMax-currentMin) + currentMin + 1);
+// };
 
-mySumbit.onclick = function() {
-    let currentGuess;
-    while (guessed === false) {
-        currentGuess = getRandomNumberMinMax();
-        if (btnTooLow.clicked == true) {
-            currentMin+= currentGuess;            
-        }
-        else if (btnTooHigh.clicked == true) {
-         currentMax-= currentGuess;   
-        }
-        else if (btnWon.clicked == true) {
-            guessed = true;
-            window.alert("YOU WON!")
-        }};
+// console.log(currentMin);
+// console.log(currentMax);
+// console.log(getRandomNumberMinMax());
+// mySumbit.onclick = function() {
+//     let currentGuess;
+//     while (guessed === false) {
+//         currentGuess = getRandomNumberMinMax();
+//         console.log(currentGuess);
+//         if (btnTooLow.clicked == true) {
+//             currentMin+= currentGuess;            
+//         }
+//         else if (btnTooHigh.clicked == true) {
+//          currentMax-= currentGuess;   
+//         }
+//         else if (btnWon.clicked == true) {
+//             guessed = true;
+//             window.alert("YOU WON!")
+//         }};
         
-};
+// };
 
 
 
@@ -84,3 +90,15 @@ mySumbit.onclick = function() {
 // let lastWord = myString.slice(myString.trim().lastIndexOf(" "));
 // charLabel.innerText = lastWord;
 
+function summ(...numbers){
+    let total = 0;
+    for (let number of numbers){
+        total += number;
+    };
+    console.log(total);
+    console.log(numbers);
+    console.log(...numbers);
+    return total;
+};
+
+sum = summ(1,2,3,4,5);
